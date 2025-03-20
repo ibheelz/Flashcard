98429d6b7a6fbd40fcc110b1943b98395a2a4b2b
ibheelz <ibheelz@icloud.com>
2025-03-20 13:25:50 +0100

    Created the window size

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+# ---------------------------- IMPORT PACKAGES ------------------------------- #
 
+import tkinter as tk
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = tk.Tk()
+window.title("Flashcard")
+window.config(bg= "skyblue")
+
+# Center the window
+WINDOW_WIDTH, WINDOW_HEIGHT = 680, 440
+x_pos = (window.winfo_screenwidth() - WINDOW_WIDTH) // 2
+y_pos = (window.winfo_screenheight() - WINDOW_HEIGHT) // 2
+window.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}+{x_pos}+{y_pos}")
+
+# canvas = tk.Canvas(window, width= 400, height= 400, bg= "white")
+
+
+
+
+window.mainloop()
